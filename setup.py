ec0b3d12de67b66319d60c35857656e4e03cf82d
Maurício Bonatte <mbonatte@ymail.com>
2022-06-29 15:43:18 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = 'secal',
+    version='0.1',
+    description='Section Analysis - modelling and analysis of custom cross-sections',
+    author = 'Maurício Bonatte',
+    author_email='mbonatte@ymail.com',
+    url = 'https://github.com/mbonatte/SectionAnalysis',
+    license='MIT',
+    long_description=open('README.md').read(),
+    
+    # Dependencies
+    install_requires=['numpy', 'matplotlib'],
+    
+    # Packaging
+    packages =[ 'secal'],
+    
+)
